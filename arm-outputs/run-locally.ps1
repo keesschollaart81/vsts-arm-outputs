a37a7aca6b8647135ee6d1eae0e3934638e7fc89
Kees Schollaart <keesschollaart81@hotmail.com>
2018-09-01 16:36:39 +0200

    merge conflict

--- arm-outputs/run-locally.ps1
+++ arm-outputs/run-locally.ps1
@@ -1,6 +1,6 @@
-#Login-AzureRmAccount -TenantId "7d25d723-384b-4d25-aaba-4e6c8b0762d5"
-Select-AzureRmSubscription -SubscriptionName "f2da982c-fc6f-4663-ad1e-46a186f9fa84"
-$resourceGroupName = "Home-Assistant-Backend"
+Login-AzureRmAccount -TenantId "7d25d723-384b-4d25-aaba-4e6c8b0762d5"
+Select-AzureRmSubscription -SubscriptionName "Kees Prive"
+$resourceGroupName = "sprint-goal-dev"
 $prefix = $null
 [Array]$outputNames = $null
 $deploymentNameFilter = $null
