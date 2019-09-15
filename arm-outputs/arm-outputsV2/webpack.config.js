eb112da0e01758d85f8cf8520b31ca1a92a2c751
Kees Schollaart <keesschollaart81@hotmail.com>
2019-09-15 13:49:26 +0200

    Revert webpack config

--- arm-outputs/arm-outputsV2/webpack.config.js
+++ arm-outputs/arm-outputsV2/webpack.config.js
@@ -14,9 +14,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
       { from: "./icon.png", to: "icon.png" },
-      { from: "./task.json", to: "task.json" },
-      { from: './node_modules/azure-pipelines-task-lib/lib.json', to: path.resolve(__dirname, 'dist', 'bundle', 'lib.json') },
-      { from: './node_modules/azure-pipelines-task-lib/Strings', to: path.resolve(__dirname, 'dist', 'bundle', 'Strings') },
+      { from: "./task.json", to: "task.json" }
     ])
   ]
 };
