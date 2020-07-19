dcd2ef37c1e82c38b5797543dae193e1d45a6bb0
Kees Schollaart <keesschollaart81@hotmail.com>
2020-07-19 11:09:12 +0200

    resources.rejson?

--- arm-outputs/arm-outputsV2/webpack.config.js
+++ arm-outputs/arm-outputsV2/webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin'); 
 
 module.exports = {
   mode: 'development',
@@ -11,9 +11,10 @@ module.exports = {
     libraryTarget: 'umd',
     filename: 'index.js'
   },
-  plugins: [
+  plugins: [ 
     new CopyPlugin({
       patterns: [
+        { from: "node_modules/azure-pipelines-task-lib/Strings", to: "Strings" },
         { from: "./icon.png", to: "icon.png" },
         { from: "./task.json", to: "task.json" }
       ]
